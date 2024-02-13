e71f066e852763db64d5526bf19d64b59d48fd5d
Maghish <mail4mithran@gmail.com>
2024-02-13 23:53:25 +0530

    Demo pt-1

--- src/components/SubHeading.jsx
+++ src/components/SubHeading.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 function SubHeading() {
     return (
-        <p className='font-mono text-black text-3xl'><Link to={props.linkTo}>{props.subHeading}</Link></p>
+        <p className='font-mono text-black text-2xl'><Link to={props.linkTo}>{props.subHeading}</Link></p>
     )
 }
 
