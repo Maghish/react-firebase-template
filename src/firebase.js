a6b58230c9e5936b3969dcddf151d60b07bf01db
Maghish <mail4mithran@gmail.com>
2024-02-13 23:50:21 +0530

    Integrated Firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app'
+
+const app = initializeApp({
+    apiKey: "AIzaSyDkeo0jvyQgDEREAlwlAvoxIgrhJdXXpF4",
+    authDomain: "react-firebase-template-a80b6.firebaseapp.com",
+    projectId: "react-firebase-template-a80b6",
+    storageBucket: "react-firebase-template-a80b6.appspot.com",
+    messagingSenderId: "744565270761",
+    appId: "1:744565270761:web:6ba70125ead1a13e1e6878"
+})
